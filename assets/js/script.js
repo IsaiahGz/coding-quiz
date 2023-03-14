6eed2dbb0d6a267525db3b7d91e0965dc659fea7
IsaiahGz <isaiahgala99@gmail.com>
2023-03-14 18:21:45 -0400

    Add initial start and end game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,5 +14,36 @@ var questionResponseEl = document.getElementById('question-response')
 var maxTime = 75
 
 // Changing Data
-var currentTime = 0
+var currentTime = maxTime
 var gameTimerId = 0
+
+// Event listeners
+startQuizBtnEl.addEventListener('click', function () {
+	// Start the game
+	gameTimerId = setInterval(gameTimerHandle, 100)
+	// Switch from start-screen to question-screen
+	startScreenEl.classList.add('hidden')
+	questionScreenEl.classList.remove('hidden')
+})
+
+// Game timer (runs every 100 ms)
+function gameTimerHandle() {
+	// Reduce time
+	currentTime -= 0.1
+	if (currentTime <= 0) {
+		// Time has run out
+		currentTime = 0 // Don't allow negative time left
+		endGame()
+	}
+	timerEl.textContent = Math.floor(currentTime)
+}
+
+// Functions
+function endGame() {
+	clearInterval(gameTimerId)
+	// Switch from question screen to end screen
+	questionScreenEl.classList.add('hidden')
+	endScreenEl.classList.remove('hidden')
+	// Set final score to time left
+	finalScoreNumberEl.textContent = currentTime
+}
