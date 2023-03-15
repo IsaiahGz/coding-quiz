91322ae086679ae72b20217d5d897f856053f5dc
IsaiahGz <isaiahgala99@gmail.com>
2023-03-15 16:43:35 -0400

    Add clear highscore event

---
+++ assets/js/highscore.js
@@ -0,0 +1,32 @@
+// Get elements
+var highscoreListEl = document.querySelector('#highscore-list')
+var clearHighscoreBtnEl = document.querySelector('#clear-highscore')
+
+// Get localstorage
+var highscoreLocalStorage = localStorage.getItem('highscores')
+
+if (highscoreLocalStorage) {
+	highscoreLocalStorage = JSON.parse(highscoreLocalStorage)
+	// Sort the array
+	highscoreLocalStorage.sort(function (a, b) {
+		if (a.score > b.score) return -1
+		else if (a.score < b.score) return 1
+		return 0
+	})
+	// Iterate scores, create LIs and push to list
+	for (let i = 0; i < highscoreLocalStorage.length; i++) {
+		let newLi = document.createElement('li')
+		newLi.textContent = `${i + 1}. ${highscoreLocalStorage[i].initials} - ${highscoreLocalStorage[i].score}`
+		highscoreListEl.appendChild(newLi)
+	}
+} else {
+	// There are no highscores in local storage
+}
+
+// Event listener
+clearHighscoreBtnEl.addEventListener('click', function () {
+	// Clear local storage
+	localStorage.setItem('highscores', '[]')
+	// Clear list display
+	highscoreListEl.textContent = ''
+})
